e01a508377c0db43b9fa2f7de3e89ab7a891d866
TAKEHITO Koshimizu <takehito.koshimizu@glpgs.com>
2017-06-01 13:22:38 +0900

    安全にアンラップ

--- RxWKWebView/WKWebView.swift
+++ RxWKWebView/WKWebView.swift
@@ -14,44 +14,36 @@ import RxCocoa
 extension Reactive where Base : WKWebView {
 
     public var title: Observable<String?> {
-        return observeWeakly(String?.self, #keyPath(WKWebView.title))
-            .map { $0! }
+        return observeWeakly(String?.self, #keyPath(WKWebView.title)).unwrapped()
     }
 
     public var url: Observable<URL?> {
-        return observeWeakly(URL?.self, #keyPath(WKWebView.url))
-            .map { $0! }
+        return observeWeakly(URL?.self, #keyPath(WKWebView.url)).unwrapped()
     }
 
     public var isLoading: Observable<Bool> {
-        return observeWeakly(Bool.self, #keyPath(WKWebView.isLoading))
-            .map { $0! }
+        return observeWeakly(Bool.self, #keyPath(WKWebView.isLoading)).unwrapped()
     }
 
     public var estimatedProgress: Observable<Double> {
-        return observeWeakly(Double.self, #keyPath(WKWebView.estimatedProgress))
-            .map { $0! }
+        return observeWeakly(Double.self, #keyPath(WKWebView.estimatedProgress)).unwrapped()
     }
 
     public var hasOnlySecureContent: Observable<Bool> {
-        return observeWeakly(Bool.self, #keyPath(WKWebView.hasOnlySecureContent))
-            .map { $0! }
+        return observeWeakly(Bool.self, #keyPath(WKWebView.hasOnlySecureContent)).unwrapped()
     }
 
     @available(iOS 10.0, *)
     public var serverTrust: Observable<SecTrust?> {
-        return observeWeakly(SecTrust?.self, #keyPath(WKWebView.serverTrust))
-            .map { $0! }
+        return observeWeakly(SecTrust?.self, #keyPath(WKWebView.serverTrust)).unwrapped()
     }
 
     public var canGoBack: Observable<Bool> {
-        return observeWeakly(Bool.self, #keyPath(WKWebView.canGoBack))
-            .map { $0! }
+        return observeWeakly(Bool.self, #keyPath(WKWebView.canGoBack)).unwrapped()
     }
 
     public var canGoForward: Observable<Bool> {
-        return observeWeakly(Bool.self, #keyPath(WKWebView.canGoForward))
-            .map { $0! }
+        return observeWeakly(Bool.self, #keyPath(WKWebView.canGoForward)).unwrapped()
     }
 
     public var goBack: UIBindingObserver<Base, Void> {
@@ -82,5 +74,25 @@ extension Reactive where Base : WKWebView {
         return UIBindingObserver(UIElement: base) { webview, _ in
             webview.stopLoading()
         }
+    }
+}
+
+fileprivate protocol OptionalType {
+    associatedtype WrapType
+
+    var isNone: Bool { get }
+    var unsafelyUnwrapped: WrapType { get }
+}
+
+extension Optional: OptionalType {
+    fileprivate typealias WrapType = Wrapped
+
+    var isNone: Bool { return self == nil }
+}
+
+extension ObservableType where E: OptionalType {
+
+    fileprivate func unwrapped() -> Observable<E.WrapType> {
+        return self.filter { !$0.isNone }.map { $0.unsafelyUnwrapped }
     }
 }
